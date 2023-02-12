41b3a857e65b907d330a9801a956c20b8530c453
ribpy <mdriazbappy22@gmail.com>
2023-02-12 13:43:53 +0600

    some changes in bs classes

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -16,15 +16,9 @@ const SearchForm=({onSubmit})=>{
      <form className="me-3" role="search" >
         <div className="input-group me-3  ">
             <input className="form-control nav-input border-end-0 border" value={input} type="search" placeholder="Search By Title or Brand" id="example-search-input" onChange={handleSearch}/>
-<<<<<<< HEAD
             <div className="input-group-append">
                     <span className="input-group-text" id="basic-addon2"><i className="icon" onClick={handleSubmit}><FaSearch/></i></span>
             </div>
-=======
-            <span className="input-group-append">
-               <span className="input-group-text" id="basic-addon2"><i className="icon" onClick={handleSubmit}><FaSearch/></i></span>
-            </span>
->>>>>>> 716c50928b87943be5547309e46acab2970824f0
         </div>
      </form>
     </>
